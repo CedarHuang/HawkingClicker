054c233348553766f3364bf87003a8588d22dc0f
CedarHuang <cedar.hxd@gmail.com>
2025-08-10 03:56:01 +0800

    改进i18n以正确处理None值

--- src/i18n/__init__.py
+++ src/i18n/__init__.py
@@ -13,8 +13,8 @@ except:
     _i18n = en.i18n
 
 def i18n(key):
-    if key in _i18n:
+    if key in _i18n and _i18n[key] is not None:
         return _i18n[key]
-    if key in en.i18n:
+    if key in en.i18n and en.i18n[key] is not None:
         return en.i18n[key]
     return key
