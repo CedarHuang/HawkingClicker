ca143a841a8f11fb0c8b6cda18bd619dd180ff22
CedarHuang <cedar.hxd@gmail.com>
2025-08-23 05:24:44 +0800

    优化日志级别设置，动态调整日志级别

--- src/logger.py
+++ src/logger.py
@@ -7,15 +7,22 @@ import common
 # 日志文件路径
 script_log_file_path = os.path.join(common.root_path(), 'scripts.log')
 
+def log_level():
+    if hasattr(sys, '_MEIPASS'):
+        return logging.INFO
+    return logging.DEBUG
+
 def create_logger(name, path):
+    level = log_level()
+
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
+    logger.setLevel(level)
 
     file_handler = logging.FileHandler(path, encoding='utf-8')
-    file_handler.setLevel(logging.INFO)
+    file_handler.setLevel(level)
 
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.INFO)
+    console_handler.setLevel(level)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
